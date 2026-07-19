25827f163f0e577fff6285ab667717d07ca7ecc5
Premkumar200531 <premkumar824839@gmail.com>
2026-07-19 08:58:23 +0530

    fix: added extend_existing to user model

--- cloud-based code compilation/models.py
+++ cloud-based code compilation/models.py
@@ -87,6 +87,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
